3420d5e21f7853cba8a147f3e03342601ea01ede
mandyyang@dofun.cn <mandyyang@dofun.cn>
2023-02-07 16:48:07 +0800

    提交配置代码

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router/index'
+import { createPinia }from 'pinia'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import * as echarts from 'echarts'
 
-createApp(App).mount('#app')
+const pinia=createPinia();
+const app=createApp(App);
+
+
+app.use(pinia);
+app.use(router);
+//全局使用echarts
+app.config.globalProperties.$echarts=echarts
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+// import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
+//自动导入vue中hook reactive ref等
+import AutoImport from 'unplugin-auto-import/vite'
+//自动导入ui组件
+import  Components  from 'unplugin-vue-components/vite'
+//ant-design-vue
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    //自定义组件名称
+    // VueSetupExtend(),
+   AutoImport({
+    imports:['vue','vue-router'],
+    dts:'src/auto-import.d.ts',
+    //ant-design-vue
+    resolvers:[AntDesignVueResolver()]
+   }),
+   Components({
+    //importStyle如果为false 样式就没了
+    resolvers:[AntDesignVueResolver({importStyle:true,resolveIcons:true})],
+   })
+
+  ],
+  //如果需要代理
+  server:{
+    host:'192.168.11.80',
+    proxy:{
+      '/api':{
+        //匹配请求路径
+        target:'', //代理的目标地址
+        changeOrigin:true,
+        // secure: true, // 是否https接口
+        // ws: true, // 是否代理websockets
+        //路径重写，后端有统一前缀(如api)就不开启，没有就开启
+        rewrite:(path)=>path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve:{
+    alias:{
+    "@":resolve(__dirname,'./src')
+    }
+  }
 })
